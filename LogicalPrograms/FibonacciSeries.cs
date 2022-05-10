ed8e4136684239ad8186c8c5502a648f1d2f498d
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-10 13:34:44 +0530

    Added to FibonacciSeries

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            Console.Write("Enter number of terms (terms > 2) up to which you want Fibonacci Series : ");
+            int terms = Convert.ToInt32(Console.ReadLine());
+            int first = 0, second = 1;
+            int next = first + second;
+            if (terms > 2)
+            {
+                Console.Write("Fibonacci series is up to " + terms + " terms is : {0}, {1}, ", first, second);
+                for (int i = 3; i <= terms; i++)
+                {
+                    if (i == terms)
+                    {
+                        Console.Write(next);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.Write(next + ", ");
+                        first = second;
+                        second = next;
+                        next = first + second;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered number is either negative or too small fo Fibonacci Series");
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Fibonacci Series");
+
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                }
+            }while(choice != 0);
+        }
+    }
+}
