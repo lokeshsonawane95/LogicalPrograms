f1f3f2326eadbd82ec80e1ea1338a827476c645a
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-11 11:27:09 +0530

    Added to VendingMachine

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,8 @@ namespace LogicalPrograms
         public static void Main(string[] args)
         {
             int choice;
+
+            //This loop will execute until user chooses 0
             do
             {
                 Console.WriteLine("\n1. Fibonacci Series");
@@ -15,7 +17,7 @@ namespace LogicalPrograms
                 Console.WriteLine("4. Reverse a number");
                 Console.WriteLine("5. Coupon numbers");
                 Console.WriteLine("6. Simulate Stopwatch");
-
+                Console.WriteLine("7. Vending Machine");
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -44,6 +46,10 @@ namespace LogicalPrograms
                     case 6:
                         StopWatch stopWatch = new StopWatch();
                         stopWatch.Watch();
+                        break;
+                    case 7:
+                        VendingMachine vendingMachine = new VendingMachine();
+                        vendingMachine.Driver();
                         break;
                 }
             }while(choice != 0);
---
+++ LogicalPrograms/VendingMachine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class VendingMachine
+    {
+        static int amount = 0;
+        static int totalNotes = 0, noteIndex = 0;
+
+        //Recursion logic as provided in the question
+        public int Calculate(int amount, int[] notes)
+        {
+            int remaining;
+            if (amount == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                //Calculation program for each note in notes array
+                if (amount >= notes[noteIndex])
+                {
+                    int calculateNotes = amount / notes[noteIndex];
+                    remaining = amount % notes[noteIndex];
+                    amount = remaining;
+                    totalNotes += calculateNotes;
+                    Console.WriteLine("Total Rs. " + notes[noteIndex] + " notes : " + calculateNotes);
+                }
+                noteIndex++;
+
+                //Recursion call
+                return Calculate(amount, notes);
+            }
+        }
+
+        //Main driver code of the program
+        public void Driver()
+        {
+            Console.Write("Enter the change in Rs. you want : ");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+            //Declaring notes array for different notes
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            VendingMachine vendingMachine = new VendingMachine();
+
+            //Calling Calculate method to perform operation
+            vendingMachine.Calculate(amount, notes);
+            Console.WriteLine("Total number of notes needed : " + totalNotes);
+        }
+    }
+}
